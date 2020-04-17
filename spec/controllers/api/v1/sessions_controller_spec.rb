35f32beb38e5b7837687338ddd110f6f0803d13e
danghanh <ldhanh1999@gmail.com>
2020-04-17 16:59:04 +0700

    Adds test  destroy session

--- spec/controllers/api/v1/sessions_controller_spec.rb
+++ spec/controllers/api/v1/sessions_controller_spec.rb
@@ -1,9 +1,13 @@
-describe "DELETE #destroy" do
-  before(:each) do
-    @user = FactoryBot.create :user
-    sign_in @user
-    delete :destroy, id: @user.auth_token
-  end
+describe Api::V1::SessionsController do
+  describe "DELETE #destroy" do
+    before(:each) do
+      @user = FactoryBot.create :user
+      sign_in @user
+      delete :destroy, params: { id: @user.auth_token }
+    end
 
-  it { should respond_with 204 }
+    it "should response 204" do
+      expect(response).to have_http_status(204)
+    end
+  end
 end
