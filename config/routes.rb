b20cd25146ceecfc88c75671c3adef7550cfc82e
danghanh <ldhanh1999@gmail.com>
2020-04-21 13:35:23 +0700

    Adds destroy session action added

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 require "api_constraints"
 
 MarketPlaceApi::Application.routes.draw do
-  devise_for :users
   namespace :api, defaults: { format: :json }, path: "/" do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
+      devise_for :users
       resources :users, :only => [:show, :create, :update, :destroy, :index]
-      resources :sessions, :only => [:create, :destroy]
     end
   end
 end
